c7d320b4b128ef9e5bca3eb32c8bd6ac18d68b75
Rachit Garg <rachitg747@gmail.com>
2020-09-02 15:04:49 +0530

    spelling mistake

--- src/arguments/search.py
+++ src/arguments/search.py
@@ -8,7 +8,7 @@ PARSER.add_argument(
     "-s", "--search", help="enable debug mode", action="store_true")
 
 ARGV = PARSER.parse_args()
-utitlity_obj = Utility()
+utility_obj = Utility()
 
 
 class Search:
@@ -19,10 +19,10 @@ class Search:
             question = input()
             print("Tags : ", end=" ")
             tags = input()
-            json_output = utitlity_obj.make_request(question, tags)
-            questions = utitlity_obj.get_que(json_output)
+            json_output = utility_obj.make_request(question, tags)
+            questions = utility_obj.get_que(json_output)
             if questions == []:
                 print(colored('No answer found,', 'red'),
                       colored('Please try reddit', 'green'))
             else:
-                utitlity_obj.get_ans(questions)
+                utility_obj.get_ans(questions)

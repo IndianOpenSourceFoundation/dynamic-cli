9c7df1b3f58a337c335615d6f846a819105559cc
aastha-jm <79910982+aastha-jm@users.noreply.github.com>
2021-03-27 21:46:51 +0530

    Add optional argument -start
    With this argument, the CLI can now introduce itself to the new user in an interesting way, giving an introduction to DYNAMIC CLI. (Just 'start' would have not worked because that would make it a positional argument in argparse which was not the desired outcome)

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ from src.arguments.search import Search
 version = "0.1"
 
 parser = argparse.ArgumentParser()
+parser.add_argument("-start",  
+                    help="introduce you to dynamic",
+                    action="store_true")
+
 parser.add_argument("-s",
                     "--search",
                     help="enable debug mode",
@@ -34,4 +38,7 @@ ARGV = parser.parse_args()
 search_flag = Search(ARGV)
 
 if __name__ == "__main__":
-    search_flag.search_args()
+    if ARGV.start:
+        print("add an introduction and emojis here")
+    else:
+        search_flag.search_args()

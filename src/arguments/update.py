014bc5838453fff0aa6bf0ba612fafb777c1d2f4
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-07 11:00:21 +0530

    Trying to fix the codacy check fails

--- src/arguments/update.py
+++ src/arguments/update.py
@@ -8,7 +8,13 @@ class UpdateApplication(object):
     def __init__(self, current_version):
         """
         Check for updates in the application
-        via github releases API
+
+        Check for updates in the application
+        via github's public api. The application
+        checks for the latest release and make
+        sure that the version of the application
+        is same as the latest release tag
+
         """
         self.current_version = current_version
         self.release_api_url = "https://api.github.com/repos/IndianOpenSourceFoundation/dynamic-cli/releases/latest
@@ -17,7 +23,6 @@ class UpdateApplication(object):
         try:
             data = requests.get(self.release_api_url)
             data = data.json()
-        
             if 'message' in data:
                 if data['message'] == "Not Found":
                     print(colored("The application do not have any release", "yellow"))
@@ -28,8 +33,6 @@ class UpdateApplication(object):
             else:
                 print(colored(f"New release found - {data.tag_name}", "red"))
                 webbrowser.open(data["html_url"])
-            
+
         except Exception as exception:
             exception = SearchError(str(exception), "Try later")
-
-    

bfce1a0811c12b030462405a28acb6c35e72e019
yasht01 <yasht.2701@gmail.com>
2021-04-09 19:41:15 +0530

    Add URL checks and endpoint checks

--- src/arguments/api_test.py
+++ src/arguments/api_test.py
@@ -1,4 +1,5 @@
 import requests, json
+import re
 
 class Api_Testing():
     default_url = "127.0.0.1:8000"
@@ -9,6 +10,17 @@ class Api_Testing():
         input_url = input('Enter URL: ')
         if input_url != '':
             request_url = input_url
+
+        has_endpoint = self.check_endpoint(request_url)
+
+        if not(has_endpoint):
+            if(request_url[-1] == '/'):
+                endpoint = input("Input endpoint (Without the starting slash): ")
+            else:
+                endpoint = input("Input endpoint (With the starting slash): ")
+            request_url += endpoint
+
+        print("Trying ...\u26A1")
 
         try:
             response = requests.get(request_url)
@@ -21,3 +33,20 @@ class Api_Testing():
             print("Check whether the URL is valid or check if the localhost server is active or not")
         except Exception as e:
             print(e)
+
+    def check_endpoint(url):
+        check_string = url
+        is_localhost_url = False
+
+        if(len(check_string.split(":")[-1]) <= 5):
+            is_localhost_url = True
+
+        if(is_localhost_url):
+            check_string = check_string.split(':')[-1]
+        else:
+            check_string = check_string.split('.')[-1]
+
+        if(len(check_string) <= 5):
+            return False
+        else:
+            return True

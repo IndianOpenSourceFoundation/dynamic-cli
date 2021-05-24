ce01b8a284184c042980feb5820bb0c2dd227754
Namami Shanker <namami2011@gmail.com>
2021-05-25 00:55:23 +0530

    Fix Codacy issues

--- src/arguments/notion.py
+++ src/arguments/notion.py
@@ -13,8 +13,38 @@ def get_token_from_cookie(cookie, token):
         if el['name'] == token:
             return el
 
+def get_token_from_file():
+    try:
+        with open(TOKEN_FILE_PATH, 'r') as f:
+            data = f.read()
+    except Exception as e:
+        print(e)
+    if(not data or data==""):
+        raise RuntimeError("Token not found in file")
+    else:
+        return data
+
+def get_cookies_from_login():
+    """
+    Provides the user browser window to login to Notion
+    Returns the user's cookies which can be used to
+    access and transfer content to user's Notion account
+    """
+    driver = get_browser_driver()
+    try:
+        driver.get(LOGIN_PATH)
+        WebDriverWait(driver, 300).until(
+            EC.presence_of_element_located((By.CLASS_NAME,
+                                            "notion-sidebar-container")))
+        return driver.get_cookies()
+    except Exception as e:
+        print(e)
+    finally:
+        driver.quit()
+
 class NotionClient():
     """
+
     Implements Login and token retrieval
 
     Handles the entire procedure of connecting to User's Notion account,
@@ -30,48 +60,19 @@ class NotionClient():
         self.tokenv2_cookie = None
         self.tokenv2_key = 'TOKENV2'
 
-    def get_token_from_file(self):
-        try:
-            with open(TOKEN_FILE_PATH, 'r') as f:
-                data = f.read()
-        except Exception as e:
-            print(e)
-        if(not data or data==""):
-            raise RuntimeError("Token not found in file")
-        else:
-            return data
-
     def save_token_file(self):
         if(self.tokenv2_cookie):
             with open(TOKEN_FILE_PATH, 'w') as f:
                 f.write(str(self.tokenv2_cookie))
 
-    def get_cookies_from_login(self):
-        """
-        Provides the user browser window to login to Notion
-        Returns the user's cookies which can be used to
-        access and transfer content to user's Notion account
-        """
-        driver = get_browser_driver()
-        try:
-            driver.get(LOGIN_PATH)
-            WebDriverWait(driver, 300).until(
-                EC.presence_of_element_located((By.CLASS_NAME,
-                                                "notion-sidebar-container")))
-            return driver.get_cookies()
-        except Exception as e:
-            print(e)
-        finally:
-            driver.quit()
-
     def get_tokenv2_cookie(self):
         # Sets 'tokenv2_cookie equal to the particular cookie containing token_v2
         if not self.tokenv2_cookie:
             try:
-                self.tokenv2_cookie = self.get_token_from_file()
+                self.tokenv2_cookie = get_token_from_file()
             except Exception:
                 try:
-                    cookies = self.get_cookies_from_login()
+                    cookies = get_cookies_from_login()
                     self.tokenv2_cookie = get_token_from_cookie(cookies, 'token_v2')
                 except Exception as e:
                     print(e)

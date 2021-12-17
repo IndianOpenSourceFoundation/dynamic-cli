2af36e6bc785bc59e539ab6906d8c272cd4f42c7
Gourav Sardana <gsardana001@gmail.com>
2021-12-17 19:25:30 +0530

    add pyproject file to make it a package and revamp setup

--- setup.py
+++ setup.py
@@ -8,12 +8,18 @@ DEPENDENCIES = []
 setuptools.setup(
     name="dynamic",
     version="0.0.1",
+    author="IOSF Community",
+    author_email="ping@iosf.in",
     description="Search for your questions in stackoverflow",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/IndianOpenSourceFoundation/dynamic-cli",
-    packages=setuptools.find_packages(),
+    project_urls={
+        "Bug Tracker": "https://github.com/IndianOpenSourceFoundation/dynamic-cli/issues",
+    },
     install_requires=DEPENDENCIES,
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"),
     entry_points={"console_scripts": ['dynamic=main:search_flag.search_args']},
     classifiers=[
         "Programming Language :: Python :: 3",
